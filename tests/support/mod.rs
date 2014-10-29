3eb1c1c93a93f96f6630341dcb6e4a63bbd42130
Tomaka17 <pierre.krieger1708@gmail.com>
2014-10-29 18:41:01 +0100

    Tests are no longer headless by default

--- tests/support/mod.rs
+++ tests/support/mod.rs
@@ -3,10 +3,15 @@ Test supports module.
 
 */
 use glutin;
-
 use glium::{mod, DisplayBuild};
+
+use std::os;
 
 /// Builds a headless display for tests.
 pub fn build_display() -> glium::Display {
-    glutin::HeadlessRendererBuilder::new(1024, 768).build_glium().unwrap()
+    if os::getenv("HEADLESS_TESTS").is_some() {
+        glutin::HeadlessRendererBuilder::new(1024, 768).build_glium().unwrap()
+    } else {
+        glutin::WindowBuilder::new().build_glium().unwrap()
+    }
 }
