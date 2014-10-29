62b2cd3790442f4f095f07a97f56040af4e49f94
Tomaka17 <pierre.krieger1708@gmail.com>
2014-10-29 09:35:04 +0100

    Add some program-related tests
    cc #15

---
+++ tests/program.rs
@@ -0,0 +1,119 @@
+#![feature(phase)]
+
+#[phase(plugin)]
+extern crate glium_macros;
+
+extern crate glutin;
+extern crate glium;
+
+use glium::DisplayBuild;
+
+#[test]
+fn program_creation() {
+    let display = glutin::HeadlessRendererBuilder::new(1024, 768)
+        .build_glium().unwrap();
+
+    // compiling shaders and linking them together
+    glium::Program::new(&display,
+        // vertex shader
+        "
+            #version 110
+
+            uniform mat4 matrix;
+
+            attribute vec2 position;
+            attribute vec3 color;
+
+            varying vec3 vColor;
+
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                vColor = color;
+            }
+        ",
+
+        // fragment shader
+        "
+            #version 110
+            varying vec3 vColor;
+
+            void main() {
+                gl_FragColor = vec4(vColor, 1.0);
+            }
+        ",
+
+        // geometry shader
+        None).unwrap();
+}
+
+#[test]
+fn program_compilation_error() {
+    let display = glutin::HeadlessRendererBuilder::new(1024, 768)
+        .build_glium().unwrap();
+
+    // compiling shaders and linking them together
+    let program = glium::Program::new(&display,
+        // vertex shader
+        "invalid glsl code",
+
+        // fragment shader
+        "
+            #version 110
+            varying vec3 vColor;
+
+            void main() {
+                gl_FragColor = vec4(vColor, 1.0);
+            }
+        ",
+
+        // geometry shader
+        None);
+
+    match program {
+        Err(glium::CompilationError(_)) => (),
+        _ => fail!()
+    };
+}
+
+/*
+This test is disabled because some OpenGL drivers don't catch
+the linking error (even though they are supposed to)
+
+#[test]
+fn program_linking_error() {
+    let display = glutin::HeadlessRendererBuilder::new(1024, 768)
+        .build_glium().unwrap();
+
+    // compiling shaders and linking them together
+    let program = glium::Program::new(&display,
+        // vertex shader
+        "
+            #version 110
+
+            varying vec3 output1;
+
+            void main() {
+                gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+                output1 = vec3(0.0, 0.0, 0.0);
+            }
+        ",
+
+        // fragment shader
+        "
+            #version 110
+            varying vec3 output2;
+
+            void main() {
+                gl_FragColor = vec4(output2, 1.0);
+            }
+        ",
+
+        // geometry shader
+        None);
+
+    match program {
+        Err(glium::LinkingError(_)) => (),
+        _ => fail!()
+    };
+}
+*/
