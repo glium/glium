e9960db5310bc49f58df317513d886fd63665ca1
Tomaka17 <pierre.krieger1708@gmail.com>
2014-10-29 09:28:45 +0100

    Fix program compilation errors not being exported

--- src/lib.rs
+++ src/lib.rs
@@ -217,6 +217,7 @@ pub use data_types::GLDataTuple;
 pub use index_buffer::IndexBuffer;
 pub use vertex_buffer::{VertexBuffer, VertexBindings, VertexFormat};
 pub use program::{Program, ProgramCreationError};
+pub use program::{CompilationError, LinkingError, ProgramCreationFailure, ShaderTypeNotSupported};
 pub use texture::{Texture, Texture2D};
 
 use std::sync::Arc;
