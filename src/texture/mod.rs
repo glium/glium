dbd5cd129733d7dbf5a6907c0050bb776ec42f83
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-12-02 22:15:15 +0100

    Minor work on pixel buffers

--- src/texture/mod.rs
+++ src/texture/mod.rs
@@ -702,24 +702,24 @@ impl<P: PixelValue> Texture3dData<P> for Vec<Vec<Vec<P>>> {
 
 /// Buffer that stores the content of a texture.
 ///
-/// The generic type represents the texture type that the buffer contains.
+/// The generic type represents the type of pixels that the buffer contains.
 ///
 /// **Note**: pixel buffers are unusable for the moment (they are not yet implemented).
 pub struct PixelBuffer<T> {
     buffer: Buffer,
 }
 
-impl<T> PixelBuffer<T> {
+impl<T> PixelBuffer<T> where T: PixelValue {
     /// Builds a new buffer with an uninitialized content.
-    pub fn new_empty(display: &super::Display, capacity: uint) -> PixelBuffer<()> {
+    pub fn new_empty(display: &super::Display, capacity: uint) -> PixelBuffer<T> {
         PixelBuffer {
             buffer: Buffer::new_empty::<buffer::PixelUnpackBuffer>(display, 1, capacity,
-                gl::DYNAMIC_READ),
+                                                                   gl::DYNAMIC_READ),
         }
     }
 
     /// Turns a `PixelBuffer<T>` into a `PixelBuffer<U>` without any check.
-    pub unsafe fn transmute<U>(self) -> PixelBuffer<U> {
+    pub unsafe fn transmute<U>(self) -> PixelBuffer<U> where U: PixelValue {
         PixelBuffer { buffer: self.buffer }
     }
 }
