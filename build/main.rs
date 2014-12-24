72a57c19d0397614ea36a17c9099d56b2d51e520
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-12-24 09:23:46 +0100

    Update for the changes in gl_generator

--- build/main.rs
+++ build/main.rs
@@ -1,3 +1,6 @@
+extern crate gl_generator;
+extern crate khronos_api;
+
 use std::os;
 use std::io::File;
 
@@ -7,4 +10,18 @@ fn main() {
     let dest = Path::new(os::getenv("OUT_DIR").unwrap());
 
     textures::build_texture_file(&mut File::create(&dest.join("textures.rs")).unwrap());
+
+
+    let mut gl_bindings = File::create(&dest.join("gl_bindings.rs")).unwrap();
+    gl_generator::generate_bindings(gl_generator::StructGenerator,
+                                    gl_generator::registry::Ns::Gl,
+                                    khronos_api::GL_XML,
+                                    vec![
+                                        "GL_EXT_direct_state_access".to_string(),
+                                        "GL_EXT_framebuffer_object".to_string(),
+                                        "GL_EXT_framebuffer_blit".to_string(),
+                                        "GL_NVX_gpu_memory_info".to_string(),
+                                        "GL_ATI_meminfo".to_string(),
+                                    ],
+                                    "4.5", "compatibility", &mut gl_bindings).unwrap();
 }
