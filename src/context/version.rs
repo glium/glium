cc5d201d54fc8359f3b5d3416cc6f291dfa0b435
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-01-28 08:30:58 +0100

    Extract GlVersion from context/mod.rs

---
+++ src/context/version.rs
@@ -0,0 +1,41 @@
+use std::cmp::Ordering;
+use std::ffi;
+use gl;
+
+/// Describes an OpenGL ctxt.version.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct GlVersion(pub u8, pub u8);
+
+impl PartialOrd for GlVersion {
+    fn partial_cmp(&self, other: &GlVersion) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for GlVersion {
+    fn cmp(&self, other: &GlVersion) -> Ordering {
+        match self.0.cmp(&other.0) {
+            Ordering::Equal => self.1.cmp(&other.1),
+            a => a
+        }
+    }
+}
+
+pub fn get_gl_version(gl: &gl::Gl) -> GlVersion {
+    unsafe {
+        let version = gl.GetString(gl::VERSION) as *const i8;
+        let version = String::from_utf8(ffi::c_str_to_bytes(&version).to_vec()).unwrap();
+
+        let version = version.words().next().expect("glGetString(GL_VERSION) returned an empty \
+                                                     string");
+
+        let mut iter = version.split(move |&mut: c: char| c == '.');
+        let major = iter.next().unwrap();
+        let minor = iter.next().expect("glGetString(GL_VERSION) did not return a correct version");
+
+        GlVersion(
+            major.parse().expect("failed to parse GL major version"),
+            minor.parse().expect("failed to parse GL minor version"),
+        )
+    }
+}
