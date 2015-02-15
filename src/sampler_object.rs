c06dc9df7cd232b60afb36dfd842e79a7c1ed5e2
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-02-15 15:47:42 +0100

    Minor changes in sampler_object.rs

--- src/sampler_object.rs
+++ src/sampler_object.rs
@@ -5,7 +5,7 @@ use std::sync::mpsc;
 use uniforms::SamplerBehavior;
 
 use gl;
-use context;
+use context::{self, GlVersion};
 use GlObject;
 use ToGlEnum;
 
@@ -16,10 +16,15 @@ pub struct SamplerObject {
 }
 
 impl SamplerObject {
+    /// Builds a new sampler object.
     pub fn new(display: &Display, behavior: &SamplerBehavior) -> SamplerObject {
-        let (tx, rx) = mpsc::channel();
+        // making sure that the backend supports samplers
+        assert!(display.context.context.get_version() >= &GlVersion(3, 2) ||
+                display.context.context.get_extensions().gl_arb_sampler_objects);
 
+        let (tx, rx) = mpsc::channel();
         let behavior = behavior.clone();
+
         display.context.context.exec(move |ctxt| {
             let sampler = unsafe {
                 use std::mem;
@@ -28,17 +33,19 @@ impl SamplerObject {
                 sampler
             };
 
+            tx.send(sampler).unwrap();
+
             unsafe {
                 ctxt.gl.SamplerParameteri(sampler, gl::TEXTURE_WRAP_S,
-                    behavior.wrap_function.0.to_glenum() as gl::types::GLint);
+                                          behavior.wrap_function.0.to_glenum() as gl::types::GLint);
                 ctxt.gl.SamplerParameteri(sampler, gl::TEXTURE_WRAP_T,
-                    behavior.wrap_function.1.to_glenum() as gl::types::GLint);
+                                          behavior.wrap_function.1.to_glenum() as gl::types::GLint);
                 ctxt.gl.SamplerParameteri(sampler, gl::TEXTURE_WRAP_R,
-                    behavior.wrap_function.2.to_glenum() as gl::types::GLint);
+                                          behavior.wrap_function.2.to_glenum() as gl::types::GLint);
                 ctxt.gl.SamplerParameteri(sampler, gl::TEXTURE_MIN_FILTER,
-                    behavior.minify_filter.to_glenum() as gl::types::GLint);
+                                          behavior.minify_filter.to_glenum() as gl::types::GLint);
                 ctxt.gl.SamplerParameteri(sampler, gl::TEXTURE_MAG_FILTER,
-                    behavior.magnify_filter.to_glenum() as gl::types::GLint);
+                                          behavior.magnify_filter.to_glenum() as gl::types::GLint);
 
                 if let Some(max_value) = ctxt.capabilities.max_texture_max_anisotropy {
                     let value = if behavior.max_anisotropy as f32 > max_value {
@@ -50,8 +57,6 @@ impl SamplerObject {
                     ctxt.gl.SamplerParameterf(sampler, gl::TEXTURE_MAX_ANISOTROPY_EXT, value);
                 }
             }
-
-            tx.send(sampler).unwrap();
         });
 
         SamplerObject {
@@ -63,6 +68,7 @@ impl SamplerObject {
 
 impl GlObject for SamplerObject {
     type Id = gl::types::GLuint;
+
     fn get_id(&self) -> gl::types::GLuint {
         self.id
     }
@@ -79,20 +85,25 @@ impl Drop for SamplerObject {
     }
 }
 
+/// Returns the sampler corresponding to the given behavior, or a draw error if
+/// samplers are not supported.
 pub fn get_sampler(display: &Display, behavior: &SamplerBehavior)
                    -> Result<gl::types::GLuint, DrawError>
 {
-    if display.context.context.get_version() <= &context::GlVersion(3, 3) &&
+    // checking for compatibility
+    if display.context.context.get_version() < &context::GlVersion(3, 2) &&
         !display.context.context.get_extensions().gl_arb_sampler_objects
     {
         return Err(DrawError::SamplersNotSupported);
     }
 
+    // looking for an existing sampler
     match display.context.samplers.lock().unwrap().get(behavior) {
         Some(obj) => return Ok(obj.get_id()),
         None => ()
     };
 
+    // builds a new sampler
     let sampler = SamplerObject::new(display, behavior);
     let id = sampler.get_id();
     display.context.samplers.lock().unwrap().insert(behavior.clone(), sampler);
