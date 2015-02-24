ab6d5bcde09cfc25f9cf4a405faa1a57a7e4c936
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-02-24 15:33:57 +0100

    Fix texture creation tests

--- tests/texture_creation.rs
+++ tests/texture_creation.rs
@@ -7,24 +7,6 @@ use glium::{Texture, Surface};
 mod support;
 
 #[test]
-fn texture_1d_creation() {    
-    let display = support::build_display();
-
-    let texture = glium::texture::Texture1d::new(&display, vec![
-        (0, 0, 0, 0),
-        (0, 0, 0, 0),
-        (0, 0, 0, 0u8),
-    ]);
-
-    assert_eq!(texture.get_width(), 3);
-    assert_eq!(texture.get_height(), None);
-    assert_eq!(texture.get_depth(), None);
-    assert_eq!(texture.get_array_size(), None);
-
-    display.assert_no_error();
-}
-
-#[test]
 fn empty_texture1d_u8u8u8u8() {
     let display = support::build_display();
 
@@ -124,33 +106,6 @@ fn empty_depth_texture2d_f32() {
 }
 
 #[test]
-fn texture_3d_creation() {    
-    let display = support::build_display();
-
-    let texture = glium::texture::Texture3d::new(&display, vec![
-        vec![
-            vec![(0, 0, 0, 0)],
-            vec![(0, 0, 0, 0)],
-        ],
-        vec![
-            vec![(0, 0, 0, 0)],
-            vec![(0, 0, 0, 0)],
-        ],
-        vec![
-            vec![(0, 0, 0, 0)],
-            vec![(0, 0, 0, 0u8)],
-        ],
-    ]);
-
-    assert_eq!(texture.get_width(), 1);
-    assert_eq!(texture.get_height(), Some(2));
-    assert_eq!(texture.get_depth(), Some(3));
-    assert_eq!(texture.get_array_size(), None);
-
-    display.assert_no_error();
-}
-
-#[test]
 fn compressed_texture_2d_creation() {
     let display = support::build_display();
 
@@ -236,7 +191,7 @@ empty_texture_test!(empty_depthtexture2darray, maybe DepthTexture2dArray, [64, 3
 // TODO: non-working
 //empty_texture_test!(empty_depthtexture3d, DepthTexture3d, [64, 32, 16], 64, Some(32), Some(16), None);
 empty_texture_test!(empty_integraltexture1d, maybe IntegralTexture1d, [64], 64, None, None, None);
-empty_texture_test!(empty_integraltexture1darray,maybe  IntegralTexture1dArray, [64, 32], 64, None, None, Some(32));
+empty_texture_test!(empty_integraltexture1darray, maybe IntegralTexture1dArray, [64, 32], 64, None, None, Some(32));
 empty_texture_test!(empty_integraltexture2d, maybe IntegralTexture2d, [64, 32], 64, Some(32), None, None);
 empty_texture_test!(empty_integraltexture2darray, maybe IntegralTexture2dArray, [64, 32, 16], 64, Some(32), None, Some(16));
 empty_texture_test!(empty_integraltexture3d, maybe IntegralTexture3d, [64, 32, 16], 64, Some(32), Some(16), None);
@@ -245,11 +200,11 @@ empty_texture_test!(empty_stenciltexture1darray, maybe StencilTexture1dArray, [6
 empty_texture_test!(empty_stenciltexture2d, maybe StencilTexture2d, [64, 32], 64, Some(32), None, None);
 empty_texture_test!(empty_stenciltexture2darray, maybe StencilTexture2dArray, [64, 32, 16], 64, Some(32), None, Some(16));
 empty_texture_test!(empty_stenciltexture3d, maybe StencilTexture3d, [64, 32, 16], 64, Some(32), Some(16), None);
-empty_texture_test!(empty_texture1d, Texture1d, [64], 64, None, None, None);
-empty_texture_test!(empty_texture1darray, Texture1dArray, [64, 32], 64, None, None, Some(32));
+empty_texture_test!(empty_texture1d, maybe Texture1d, [64], 64, None, None, None);
+empty_texture_test!(empty_texture1darray, maybe Texture1dArray, [64, 32], 64, None, None, Some(32));
 empty_texture_test!(empty_texture2d, Texture2d, [64, 32], 64, Some(32), None, None);
-empty_texture_test!(empty_texture2darray, Texture2dArray, [64, 32, 16], 64, Some(32), None, Some(16));
-empty_texture_test!(empty_texture3d, Texture3d, [64, 32, 16], 64, Some(32), Some(16), None);
+empty_texture_test!(empty_texture2darray, maybe Texture2dArray, [64, 32, 16], 64, Some(32), None, Some(16));
+empty_texture_test!(empty_texture3d, maybe Texture3d, [64, 32, 16], 64, Some(32), Some(16), None);
 empty_texture_test!(empty_unsignedtexture1d, maybe UnsignedTexture1d, [64], 64, None, None, None);
 empty_texture_test!(empty_unsignedtexture1darray, maybe UnsignedTexture1dArray, [64, 32], 64, None, None, Some(32));
 empty_texture_test!(empty_unsignedtexture2d, maybe UnsignedTexture2d, [64, 32], 64, Some(32), None, None);
