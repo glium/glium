0d46b8e389d1345637e85084667068ec253dc0cc
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-03-05 16:00:20 +0100

    Add example with the code in tutorial 01

---
+++ examples/tutorial-01.rs
@@ -0,0 +1,55 @@
+#[macro_use]
+extern crate glium;
+extern crate glutin;
+
+fn main() {
+    use glium::{DisplayBuild, Surface};
+    let display = glutin::WindowBuilder::new().build_glium().unwrap();
+
+    #[derive(Copy)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, shape);
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 110
+
+        attribute vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 110
+
+        void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    loop {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                    &std::default::Default::default()).unwrap();
+        target.finish();
+
+        if display.is_closed() {
+            break;
+        }
+    }
+}
