0223462600269b08671bf43832ac20a32edda750
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-03-18 14:15:52 +0100

    Cleanup sync, and OpenGL ES compatibility

--- src/sync.rs
+++ src/sync.rs
@@ -114,7 +114,9 @@ pub unsafe fn new_linear_sync_fence(ctxt: &mut context::CommandContext) -> Linea
 }
 
 pub unsafe fn new_linear_sync_fence_if_supported(ctxt: &mut context::CommandContext) -> Option<LinearSyncFence> {
-    if ctxt.version < &context::GlVersion(Api::Gl, 3, 2) && !ctxt.extensions.gl_arb_sync {
+    if !(ctxt.version >= &context::GlVersion(Api::Gl, 3, 2)) && !ctxt.extensions.gl_arb_sync
+        && !(ctxt.version >= &context::GlVersion(Api::GlEs, 3, 0))
+    {
         return None;
     }
 
@@ -124,23 +126,24 @@ pub unsafe fn new_linear_sync_fence_if_supported(ctxt: &mut context::CommandCont
 }
 
 /// Waits for this fence and destroys it, from within the commands context.
-pub unsafe fn wait_linear_sync_fence_and_drop(mut fence: LinearSyncFence, ctxt: &mut context::CommandContext) {
+pub unsafe fn wait_linear_sync_fence_and_drop(mut fence: LinearSyncFence,
+                                              ctxt: &mut context::CommandContext)
+{
     let fence = fence.id.take().unwrap();
 
     // we try waiting without flushing first
-    let success = match ctxt.gl.ClientWaitSync(fence, 0, 0) {
-        gl::ALREADY_SIGNALED => true,
-        gl::TIMEOUT_EXPIRED => false,
-        gl::WAIT_FAILED => false,
-        gl::CONDITION_SATISFIED => true,
+    match ctxt.gl.ClientWaitSync(fence, 0, 0) {
+        gl::ALREADY_SIGNALED | gl::CONDITION_SATISFIED => {
+            ctxt.gl.DeleteSync(fence);
+            return;
+        },
+        gl::TIMEOUT_EXPIRED => (),
+        gl::WAIT_FAILED => (),
         _ => unreachable!()
     };
 
     // waiting *with* flushing this time
-    if !success {
-        ctxt.gl.ClientWaitSync(fence, gl::SYNC_FLUSH_COMMANDS_BIT,
-                               365 * 24 * 3600 * 1000 * 1000 * 1000);
-    }
-
+    ctxt.gl.ClientWaitSync(fence, gl::SYNC_FLUSH_COMMANDS_BIT,
+                           365 * 24 * 3600 * 1000 * 1000 * 1000);
     ctxt.gl.DeleteSync(fence);
 }
