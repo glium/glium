90851c95da7164e319390d31355d4ecb89bc8030
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-04-13 16:50:07 +0200

    Implement view matrix

--- examples/support/camera.rs
+++ examples/support/camera.rs
@@ -1,11 +1,15 @@
 pub struct CameraState {
     aspect_ratio: f32,
+    position: (f32, f32, f32),
+    target: (f32, f32, f32),
 }
 
 impl CameraState {
     pub fn new() -> CameraState {
         CameraState {
             aspect_ratio: 1024.0 / 768.0,
+            position: (0.1, 0.1, 1.0),
+            target: (0.0, 0.0, 0.0),
         }
     }
 
@@ -26,12 +30,38 @@ impl CameraState {
     }
 
     pub fn get_view(&self) -> [[f32; 4]; 4] {
+        let f = (self.target.0 - self.position.0,
+                 self.target.1 - self.position.1,
+                 self.target.2 - self.position.2);
+
+        let f = {
+            let len = f.0 * f.0 + f.1 * f.1 + f.2 * f.2;
+            let len = len.sqrt();
+            (f.0 / len, f.1 / len, f.2 / len)
+        };
+
+        let up = (0.0, 1.0, 0.0);
+
+        let s = (f.1 * up.2 - f.2 * up.1,
+                 f.2 * up.0 - f.0 * up.2,
+                 f.0 * up.1 - f.1 * up.0);
+
+        let s_norm = {
+            let len = s.0 * s.0 + s.1 * s.1 + s.2 * s.2;
+            let len = len.sqrt();
+            (s.0 / len, s.1 / len, s.2 / len)
+        };
+
+        let u = (s_norm.1 * f.2 - s_norm.2 * f.1,
+                 s_norm.2 * f.0 - s_norm.0 * f.2,
+                 s_norm.0 * f.1 - s_norm.1 * f.0);
+
         // note: remember that this is column-major, so the lines of code are actually columns
         [
-            [1.0,    0.0,              0.0              ,   0.0],
-            [         0.0         ,     1.0 ,              0.0              ,   0.0],
-            [         0.0         ,    0.0,  1.0    ,  0.0],
-            [         0.0         ,    0.0,  -1.0,   1.0],
+            [s.0, u.0, -f.0, 0.0],
+            [s.1, u.1, -f.1, 0.0],
+            [s.2, u.2, -f.2, 0.0],
+            [-self.position.0, -self.position.1, -self.position.2, 1.0],
         ]
     }
 }
