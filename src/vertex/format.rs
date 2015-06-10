545fbbb20c16c6e48a56848fd048b95f522e5a4d
Denilson M. Amorim <dma_2012@hotmail.com>
2015-06-10 20:12:41 -0300

    Attribute trait for cgmath types

--- src/vertex/format.rs
+++ src/vertex/format.rs
@@ -2,6 +2,9 @@ use std::borrow::Cow;
 use std::mem;
 use vertex::Attribute;
 
+#[cfg(feature = "cgmath")]
+use cgmath;
+
 #[allow(missing_docs)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum AttributeType {
@@ -577,5 +580,415 @@ unsafe impl Attribute for [[f64; 3]; 3] {
 unsafe impl Attribute for [[f64; 4]; 4] {
     fn get_type() -> AttributeType {
         AttributeType::F64x4x4
+    }
+}
+
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point2<i8> {
+    fn get_type() -> AttributeType {
+        AttributeType::I8I8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point3<i8> {
+    fn get_type() -> AttributeType {
+        AttributeType::I8I8I8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector2<i8> {
+    fn get_type() -> AttributeType {
+        AttributeType::I8I8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector3<i8> {
+    fn get_type() -> AttributeType {
+        AttributeType::I8I8I8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector4<i8> {
+    fn get_type() -> AttributeType {
+        AttributeType::I8I8I8I8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point2<u8> {
+    fn get_type() -> AttributeType {
+        AttributeType::U8U8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point3<u8> {
+    fn get_type() -> AttributeType {
+        AttributeType::U8U8U8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector2<u8> {
+    fn get_type() -> AttributeType {
+        AttributeType::U8U8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector3<u8> {
+    fn get_type() -> AttributeType {
+        AttributeType::U8U8U8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector4<u8> {
+    fn get_type() -> AttributeType {
+        AttributeType::U8U8U8U8
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point2<i16> {
+    fn get_type() -> AttributeType {
+        AttributeType::I16I16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point3<i16> {
+    fn get_type() -> AttributeType {
+        AttributeType::I16I16I16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector2<i16> {
+    fn get_type() -> AttributeType {
+        AttributeType::I16I16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector3<i16> {
+    fn get_type() -> AttributeType {
+        AttributeType::I16I16I16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector4<i16> {
+    fn get_type() -> AttributeType {
+        AttributeType::I16I16I16I16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point2<u16> {
+    fn get_type() -> AttributeType {
+        AttributeType::U16U16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point3<u16> {
+    fn get_type() -> AttributeType {
+        AttributeType::U16U16U16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector2<u16> {
+    fn get_type() -> AttributeType {
+        AttributeType::U16U16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector3<u16> {
+    fn get_type() -> AttributeType {
+        AttributeType::U16U16U16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector4<u16> {
+    fn get_type() -> AttributeType {
+        AttributeType::U16U16U16U16
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point2<i32> {
+    fn get_type() -> AttributeType {
+        AttributeType::I32I32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point3<i32> {
+    fn get_type() -> AttributeType {
+        AttributeType::I32I32I32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector2<i32> {
+    fn get_type() -> AttributeType {
+        AttributeType::I32I32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector3<i32> {
+    fn get_type() -> AttributeType {
+        AttributeType::I32I32I32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector4<i32> {
+    fn get_type() -> AttributeType {
+        AttributeType::I32I32I32I32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point2<u32> {
+    fn get_type() -> AttributeType {
+        AttributeType::U32U32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point3<u32> {
+    fn get_type() -> AttributeType {
+        AttributeType::U32U32U32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector2<u32> {
+    fn get_type() -> AttributeType {
+        AttributeType::U32U32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector3<u32> {
+    fn get_type() -> AttributeType {
+        AttributeType::U32U32U32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector4<u32> {
+    fn get_type() -> AttributeType {
+        AttributeType::U32U32U32U32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point2<f32> {
+    fn get_type() -> AttributeType {
+        AttributeType::F32F32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point3<f32> {
+    fn get_type() -> AttributeType {
+        AttributeType::F32F32F32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector2<f32> {
+    fn get_type() -> AttributeType {
+        AttributeType::F32F32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector3<f32> {
+    fn get_type() -> AttributeType {
+        AttributeType::F32F32F32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector4<f32> {
+    fn get_type() -> AttributeType {
+        AttributeType::F32F32F32F32
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Matrix2<f32> {
+    fn get_type() -> AttributeType {
+        AttributeType::F32x2x2
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Matrix3<f32> {
+    fn get_type() -> AttributeType {
+        AttributeType::F32x3x3
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Matrix4<f32> {
+    fn get_type() -> AttributeType {
+        AttributeType::F32x4x4
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point2<f64> {
+    fn get_type() -> AttributeType {
+        AttributeType::F64F64
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Point3<f64> {
+    fn get_type() -> AttributeType {
+        AttributeType::F64F64F64
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector2<f64> {
+    fn get_type() -> AttributeType {
+        AttributeType::F64F64
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector3<f64> {
+    fn get_type() -> AttributeType {
+        AttributeType::F64F64F64
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Vector4<f64> {
+    fn get_type() -> AttributeType {
+        AttributeType::F64F64F64F64
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Matrix2<f64> {
+    fn get_type() -> AttributeType {
+        AttributeType::F64x2x2
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Matrix3<f64> {
+    fn get_type() -> AttributeType {
+        AttributeType::F64x3x3
+    }
+}
+
+#[cfg(feature="cgmath")]
+unsafe impl Attribute for cgmath::Matrix4<f64> {
+    fn get_type() -> AttributeType {
+        AttributeType::F64x4x4
+    }
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use std::mem;
+
+    #[cfg(feature="cgmath")]
+    #[test]
+    fn test_cgmath_layout() {
+        use cgmath::{self, FixedArray};
+
+        macro_rules! test_cgmath_layout {
+            ($from_fixed_ref:path, $ety:ty, $ncomps:expr, $literal:expr) => {{
+                // from_fixed_ref is used instead of from_fixed because the later is not yet
+                // implemented due to rust-lang/rust#16418
+                let vaa = $literal.clone();
+                let val = $from_fixed_ref(&vaa);
+                let arr: &[$ety; $ncomps] = unsafe { mem::transmute(val) };
+                assert_eq!(*arr, $literal);
+            }}
+        }
+
+        test_cgmath_layout!(cgmath::Vector2::from_fixed_ref, u8, 2, [0u8, 1]);
+        test_cgmath_layout!(cgmath::Vector3::from_fixed_ref, u8, 3, [0u8, 1, 2]);
+        test_cgmath_layout!(cgmath::Vector4::from_fixed_ref, u8, 4, [0u8, 1, 2, 3]);
+        test_cgmath_layout!(cgmath::Vector2::from_fixed_ref, i8, 2, [0i8, 1]);
+        test_cgmath_layout!(cgmath::Vector3::from_fixed_ref, i8, 3, [0i8, 1, 2]);
+        test_cgmath_layout!(cgmath::Vector4::from_fixed_ref, i8, 4, [0i8, 1, 2, 3]);
+        test_cgmath_layout!(cgmath::Point2::from_fixed_ref, u8, 2, [0u8, 1]);
+        test_cgmath_layout!(cgmath::Point3::from_fixed_ref, u8, 3, [0u8, 1, 2]);
+        test_cgmath_layout!(cgmath::Point2::from_fixed_ref, i8, 2, [0i8, 1]);
+        test_cgmath_layout!(cgmath::Point3::from_fixed_ref, i8, 3, [0i8, 1, 2]);
+
+        test_cgmath_layout!(cgmath::Vector2::from_fixed_ref, u16, 2, [0u16, 1]);
+        test_cgmath_layout!(cgmath::Vector3::from_fixed_ref, u16, 3, [0u16, 1, 2]);
+        test_cgmath_layout!(cgmath::Vector4::from_fixed_ref, u16, 4, [0u16, 1, 2, 3]);
+        test_cgmath_layout!(cgmath::Vector2::from_fixed_ref, i16, 2, [0i16, 1]);
+        test_cgmath_layout!(cgmath::Vector3::from_fixed_ref, i16, 3, [0i16, 1, 2]);
+        test_cgmath_layout!(cgmath::Vector4::from_fixed_ref, i16, 4, [0i16, 1, 2, 3]);
+        test_cgmath_layout!(cgmath::Point2::from_fixed_ref, u16, 2, [0u16, 1]);
+        test_cgmath_layout!(cgmath::Point3::from_fixed_ref, u16, 3, [0u16, 1, 2]);
+        test_cgmath_layout!(cgmath::Point2::from_fixed_ref, i16, 2, [0i16, 1]);
+        test_cgmath_layout!(cgmath::Point3::from_fixed_ref, i16, 3, [0i16, 1, 2]);
+
+        test_cgmath_layout!(cgmath::Vector2::from_fixed_ref, u32, 2, [0u32, 1]);
+        test_cgmath_layout!(cgmath::Vector3::from_fixed_ref, u32, 3, [0u32, 1, 2]);
+        test_cgmath_layout!(cgmath::Vector4::from_fixed_ref, u32, 4, [0u32, 1, 2, 3]);
+        test_cgmath_layout!(cgmath::Vector2::from_fixed_ref, i32, 2, [0i32, 1]);
+        test_cgmath_layout!(cgmath::Vector3::from_fixed_ref, i32, 3, [0i32, 1, 2]);
+        test_cgmath_layout!(cgmath::Vector4::from_fixed_ref, i32, 4, [0i32, 1, 2, 3]);
+        test_cgmath_layout!(cgmath::Point2::from_fixed_ref, u32, 2, [0u32, 1]);
+        test_cgmath_layout!(cgmath::Point3::from_fixed_ref, u32, 3, [0u32, 1, 2]);
+        test_cgmath_layout!(cgmath::Point2::from_fixed_ref, i32, 2, [0i32, 1]);
+        test_cgmath_layout!(cgmath::Point3::from_fixed_ref, i32, 3, [0i32, 1, 2]);
+
+        test_cgmath_layout!(cgmath::Vector2::from_fixed_ref, f32, 2, [0.0f32, 1.0]);
+        test_cgmath_layout!(cgmath::Vector3::from_fixed_ref, f32, 3, [0.0f32, 1.0, 2.0]);
+        test_cgmath_layout!(cgmath::Vector4::from_fixed_ref, f32, 4, [0.0f32, 1.0, 2.0, 3.0]);
+        test_cgmath_layout!(cgmath::Vector2::from_fixed_ref, f64, 2, [0.0f64, 1.0]);
+        test_cgmath_layout!(cgmath::Vector3::from_fixed_ref, f64, 3, [0.0f64, 1.0, 2.0]);
+        test_cgmath_layout!(cgmath::Vector4::from_fixed_ref, f64, 4, [0.0f64, 1.0, 2.0, 3.0]);
+        test_cgmath_layout!(cgmath::Point2::from_fixed_ref, f32, 2, [0.0f32, 1.0]);
+        test_cgmath_layout!(cgmath::Point3::from_fixed_ref, f32, 3, [0.0f32, 1.0, 2.0]);
+        test_cgmath_layout!(cgmath::Point2::from_fixed_ref, f64, 2, [0.0f64, 1.0]);
+        test_cgmath_layout!(cgmath::Point3::from_fixed_ref, f64, 3, [0.0f64, 1.0, 2.0]);
+        test_cgmath_layout!(cgmath::Matrix2::from_fixed_ref, [f32; 2], 2, [[0.0f32, 1.0],
+                                                                           [2.0f32, 3.0]]);
+        test_cgmath_layout!(cgmath::Matrix3::from_fixed_ref, [f32; 3], 3, [[0.0f32, 1.0, 2.0],
+                                                                           [3.0f32, 4.0, 5.0],
+                                                                           [6.0f32, 7.0, 8.0]]);
+        test_cgmath_layout!(cgmath::Matrix4::from_fixed_ref, [f32; 4], 4, [[0.0f32, 1.0, 2.0, 3.0],
+                                                                           [4.0f32, 5.0, 6.0, 7.0],
+                                                                           [8.0f32, 9.0, 10.0, 11.0],
+                                                                           [12.0f32, 13.0, 14.0, 15.0]]);
+        test_cgmath_layout!(cgmath::Matrix2::from_fixed_ref, [f64; 2], 2, [[0.0f64, 1.0],
+                                                                           [2.0f64, 3.0]]);
+        test_cgmath_layout!(cgmath::Matrix3::from_fixed_ref, [f64; 3], 3, [[0.0f64, 1.0, 2.0],
+                                                                           [3.0f64, 4.0, 5.0],
+                                                                           [6.0f64, 7.0, 8.0]]);
+        test_cgmath_layout!(cgmath::Matrix4::from_fixed_ref, [f64; 4], 4, [[0.0f64, 1.0, 2.0, 3.0],
+                                                                           [4.0f64, 5.0, 6.0, 7.0],
+                                                                           [8.0f64, 9.0, 10.0, 11.0],
+                                                                           [12.0f64, 13.0, 14.0, 15.0]]);
     }
 }
