e84186d4e1c5e44211acf0351f1813d35ff94bb6
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-10-24 18:09:12 +0200

    Add DefaultFramebuffer and DefaultFramebufferAttachment

--- src/framebuffer/mod.rs
+++ src/framebuffer/mod.rs
@@ -89,12 +89,14 @@ use DrawError;
 
 use {fbo, gl};
 
+pub use self::default_fb::{DefaultFramebufferAttachment, DefaultFramebuffer};
 pub use self::render_buffer::{RenderBuffer, RenderBufferAny, DepthRenderBuffer};
 pub use self::render_buffer::{StencilRenderBuffer, DepthStencilRenderBuffer};
 pub use self::render_buffer::CreationError as RenderBufferCreationError;
 pub use fbo::is_dimensions_mismatch_supported;
 pub use fbo::ValidationError;
 
+mod default_fb;
 mod render_buffer;
 
 /// A framebuffer which has only one color attachment.
