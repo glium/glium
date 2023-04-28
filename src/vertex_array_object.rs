9045bc1e9e4bbcc149be29078f28921ac413cd28
Nikolay Arhipov <n@arhipov.net>
2023-04-28 22:44:38 +0300

    PS Vita support (#2055)
    * Internally on PS Vita all vertex attributes are assumed as [float;4], so disable the attribute type check for Vita
    * Uniform location check used to check if it is >=0. The spec technically only defines -1 as an error value. Vita GLES library uses negative values there.

--- src/vertex_array_object.rs
+++ src/vertex_array_object.rs
@@ -277,6 +277,9 @@ impl VertexArrayObject {
                     }
                 };
 
+                // Unfortunately internal API used by GLES implementation on Vita
+                // assumes all attributes as float4, so we should skip this check for it.
+                #[cfg(not(target_os = "vita"))]
                 if ty.get_num_components() != attribute.ty.get_num_components() ||
                     attribute.size != 1
                 {
