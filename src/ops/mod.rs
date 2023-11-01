ba151891545a1fd3214b6b339e7c96cc3fd89aa4
est31 <MTest31@outlook.com>
2023-11-01 14:06:21 +0100

    Remove unused Destination export
    Gives a warning on latest nightly

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -1,7 +1,7 @@
 pub use self::blit::blit;
 pub use self::clear::clear;
 pub use self::draw::draw;
-pub use self::read::{read, ReadError, Source, Destination};
+pub use self::read::{read, ReadError, Source};
 
 mod blit;
 mod clear;
